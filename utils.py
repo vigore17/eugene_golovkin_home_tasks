ee3decbb0c0049e3f8fe9f10cce7ba017f9c367d
vigore17 <vigore-gl@mail.ru>
2022-06-06 22:04:19 +0300

    add files lesson2

---
+++ utils.py
@@ -0,0 +1,9 @@
+import requests
+
+def currency_rates(pare):
+    currency_page = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
+    if pare not in currency_page:
+        return None
+
+    native_currency = currency_page[currency_page.find('<Value>', currency_page.find(pare)) + 7:currency_page.find('</Value>', currency_page.find(pare))]
+    return f"{native_currency}"
